55f590e21a663e2a024d435f49bb95b998a3a17c
jaisurya <jsurya787@gmail.com>
2025-07-16 19:54:03 +0530

    space added

--- src/main/java/com/testingWeb/user/paymentService.java
+++ src/main/java/com/testingWeb/user/paymentService.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class paymentService {
     public void amount(Double amount){
+
         System.out.println("Amout is here " + amount);
+
 
     }
 }
